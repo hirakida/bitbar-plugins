7b9746072ac852f44a11c2681be668c28d66f1ba
hirakida <hirakida29@gmail.com>
2018-07-12 10:39:40 +0900

    use urllib

--- ExchangeRate/fixer.2h.py
+++ ExchangeRate/fixer.2h.py
@@ -4,8 +4,10 @@
 
 import configparser
 import os
-import requests
-from requests.exceptions import ConnectionError
+import json
+import urllib.request
+import urllib.parse
+import urllib.error
 
 TOP = "JPY"
 FILE_NAME = "~/.bitbarrc"
@@ -24,19 +26,24 @@ def load_access_key():
 
 def main():
     access_key = load_access_key()
+    url = "http://data.fixer.io/api/latest"
+    params = urllib.parse.urlencode({"access_key": access_key})
+    req = urllib.request.Request("{}?{}".format(url, params))
     try:
-        response = requests.get("http://data.fixer.io/api/latest", params={"access_key": access_key})
-    except ConnectionError as e:
-        print(e.strerror)
-        return
-    content = response.json()
-    rates = content["rates"]
-    print("{0}: {1}".format(TOP, rates[TOP]))
-    print("---")
-    print("base: {0}".format(content["base"]))
-    print("---")
-    for key in rates.keys():
-        print("{0}: {1}".format(key, rates[key]))
+        with urllib.request.urlopen(req) as response:
+            body = response.read().decode("utf8")
+            content = json.loads(body)
+            rates = content["rates"]
+            print("{0}: {1}".format(TOP, rates[TOP]))
+            print("---")
+            print("base: {0}".format(content["base"]))
+            print("---")
+            for key in rates.keys():
+                print("{0}: {1}".format(key, rates[key]))
+    except urllib.error.HTTPError as err:
+        print(err.reason)
+    except urllib.error.URLError as err:
+        print(err.reason)
 
 
 if __name__ == '__main__':
--- ip.3h.py
+++ ip.3h.py
@@ -1,31 +1,34 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
-import requests
-from requests.exceptions import ConnectionError
+import json
+import urllib.request
+import urllib.parse
+import urllib.error
 
 
 def main():
     try:
-        response = requests.get("http://ip-api.com/json")
-    except ConnectionError as e:
-        print(e.strerror)
-        return
-    content = response.json()
-    print(content["query"])
-    print("---")
-    print("as: {0}".format(content["as"]))
-    print("isp: {0}".format(content["isp"]))
-    print("org: {0}".format(content["org"]))
-    print("---")
-    print("city: {0}".format(content["city"]))
-    print("region: {0}".format(content["regionName"]))
-    print("country: {0}".format(content["country"]))
-    print("timezone: {0}".format(content["timezone"]))
-    print("zip: {0}".format(content["zip"]))
-    print("lat: {0}".format(content["lat"]))
-    print("lon: {0}".format(content["lon"]))
-    print("---")
+        with urllib.request.urlopen("http://ip-api.com/json") as response:
+            content = json.loads(response.read().decode("utf8"))
+            print(content["query"])
+            print("---")
+            print("as: {0}".format(content["as"]))
+            print("isp: {0}".format(content["isp"]))
+            print("org: {0}".format(content["org"]))
+            print("---")
+            print("city: {0}".format(content["city"]))
+            print("region: {0}".format(content["regionName"]))
+            print("country: {0}".format(content["country"]))
+            print("timezone: {0}".format(content["timezone"]))
+            print("zip: {0}".format(content["zip"]))
+            print("lat: {0}".format(content["lat"]))
+            print("lon: {0}".format(content["lon"]))
+            print("---")
+    except urllib.error.HTTPError as err:
+        print(err.reason)
+    except urllib.error.URLError as err:
+        print(err.reason)
 
 
 if __name__ == '__main__':
