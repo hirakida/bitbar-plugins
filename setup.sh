66d40ad2c0b53d08e2f0aeafc03dd1fc3ab18a62
hirakida <hirakida29@gmail.com>
2020-08-01 21:24:07 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -2,9 +2,7 @@
 
 PLUGINS_DIR="$HOME"/bitbar
 
-#cp -n "$PWD"/.bitbarrc "${PLUGINS_DIR}"/.bitbarrc
-
-PLUGINS=(py/livedoor-weather.3h.py)
+PLUGINS=(livedoor-weather.3h.py)
 for plugin in "${PLUGINS[@]}"; do
   ln -fs "$PWD"/plugins/"$plugin" "$PLUGINS_DIR"/
 done
